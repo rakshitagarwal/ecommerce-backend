299bf1b1db57eab764d826f52aa4e12dc73a7eee
rakshitagarwal <rakshitag13@gmail.com>
2023-01-02 22:34:24 +0530

    Revert "task1"
    This reverts commit 75088ce7282d4417f11b0a7de44c19308b3d4aee.

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,24 +1,27 @@
-const getDb = require("../util/database").getDb;
+const Sequelize = require('sequelize');
 
-class Product {
-  constructor(title, price, imageUrl, description) {
-    this.title = title;
-    this.price = price;
-    this.imageUrl = imageUrl;
-    this.description = description;
-  }
+const sequelize = require('../util/database');
 
-  save() {
-    const db = getDb();
-    return db.collection("products")
-      .insertOne(this)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
-}
+});
 
 module.exports = Product;
